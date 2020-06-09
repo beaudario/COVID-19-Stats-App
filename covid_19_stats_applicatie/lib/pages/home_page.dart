2624960535483944cba5481632a830dd5b600e7d
beaudario <dario.douma@upcmail.nl>
2020-06-09 22:11:34 +0200

    added custom navigation bar

--- covid_19_stats_applicatie/lib/pages/home_page.dart
+++ covid_19_stats_applicatie/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:covid_19_stats_applicatie/widgets/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,10 +6,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text("COVID-19 Statistics")
+        title: Text("COVID-19 Statistics"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Text("Hallo"),
+              ],
+            ),
+          ),
         ),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 }
---
+++ covid_19_stats_applicatie/lib/widgets/custom_navigation_bar.dart
@@ -0,0 +1,26 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class CustomNavigationBar extends StatelessWidget {
+  const CustomNavigationBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CurvedNavigationBar(
+      color: Colors.blue,
+      backgroundColor: Colors.grey[200],
+      buttonBackgroundColor: Colors.blue,
+      height: 55,
+      items: <Widget>[
+        Icon(Icons.person, color: Colors.white),
+        Icon(Icons.home, color: Colors.white),
+        Icon(Icons.favorite, color: Colors.white),
+      ],
+      animationDuration: Duration(
+          milliseconds: 350
+      ),
+    );
+  }
+}
