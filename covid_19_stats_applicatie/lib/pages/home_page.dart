ee43481f9882f27bf272efa45a008ca7a80def73
beaudario <dario.douma@upcmail.nl>
2020-06-09 22:19:11 +0200

    done for tonight

--- covid_19_stats_applicatie/lib/pages/home_page.dart
+++ covid_19_stats_applicatie/lib/pages/home_page.dart
@@ -17,7 +17,9 @@ class HomePage extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Text("Hallo"),
+                ListTile(
+                  leading: Icon(Icons.),
+                ),
               ],
             ),
           ),
